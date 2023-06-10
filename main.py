3e0c76cbdd67dfa323aa8695bc68748b0c6e26a0
Ziv Kaplan <zivkaplan@gmail.com>
2023-06-10 11:51:57 +0300

    formating: snake case

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 from wordle import Wordle
 
-def ParseChoice(choice):
+
+def parse_choice(choice):
     while True:
         choice = choice.upper()
-        if choice in ["Y","YES"]:
+        if choice in ["Y", "YES"]:
             return True
-        elif choice in ["N","NO"]:
+        elif choice in ["N", "NO"]:
             return False
         else:
             choice = input("invalid input. Try again: ")
-            
-    
+
 
 def main():
-    
     game = Wordle()
     is_playing = True
     while is_playing:
-        game.Start()
-        is_playing = ParseChoice(input("Would you like to play again? [Y/n]: "))
-        
+        game.start()
+        is_playing = parse_choice(input("Would you like to play again? [Y/n]: "))
 
 
 if __name__ == "__main__":
--- wordle/wordle.py
+++ wordle/wordle.py
@@ -2,12 +2,14 @@ import string
 from random import randint
 import os
 
-def _ClearScreen():
+
+def _clear_screen():
     clear_command = "clear"
     if os.name == 'nt':
         clear_command = "cls"
-        
+
     os.system(clear_command)
+
 
 class _Colors:
     RESET = "\033[0m"
@@ -19,13 +21,13 @@ class _Colors:
     MAGENTA = "\033[35m"
     CYAN = "\033[36m"
     WHITE = "\033[37m"
-    BOLD_BLACK = "\033[1m\033[30m"      
-    BOLD_RED = "\033[1m\033[31m"      
-    BOLD_GREEN = "\033[1m\033[32m"      
-    BOLD_YELLOW = "\033[1m\033[33m"      
-    BOLD_BLUE = "\033[1m\033[34m"      
-    BOLD_MAGENTA = "\033[1m\033[35m"      
-    BOLD_CYAN = "\033[1m\033[36m"      
+    BOLD_BLACK = "\033[1m\033[30m"
+    BOLD_RED = "\033[1m\033[31m"
+    BOLD_GREEN = "\033[1m\033[32m"
+    BOLD_YELLOW = "\033[1m\033[33m"
+    BOLD_BLUE = "\033[1m\033[34m"
+    BOLD_MAGENTA = "\033[1m\033[35m"
+    BOLD_CYAN = "\033[1m\033[36m"
     BOLD_WHITE = "\033[1m\033[37m"
     DIMMED = "\x1b[2m"
 
@@ -34,31 +36,31 @@ class _Letter:
     def __init__(self, letter, color=_Colors.BOLD_WHITE):
         self._letter = letter
         self._color = color
-    
-    def Print(self, end="\n"):
+
+    def print(self, end="\n"):
         print(f"{self._color + self._letter + _Colors.RESET}", end=end)
 
-
-    def ChangeColor(self, color):
+    def set_color(self, color):
         self._color = color
 
 
 class _Dict:
     def __init__(self):
-        self._content = self._LoadDict()
+        self._content = self._load_dict()
 
-    def _LoadDict(self):
+    @staticmethod
+    def _load_dict():
         file_location = os.path.join(
             os.path.dirname(os.path.abspath(__file__)),
             "words.txt")
-        
+
         with open(file_location, "r") as dict:
             return [line.split()[0].upper() for line in dict]
 
-    def GetRandomWord(self):
-        return self._content[randint(0, len(self._content) -1)]
+    def get_random_word(self):
+        return self._content[randint(0, len(self._content) - 1)]
 
-    def Exists(self, key):
+    def exists(self, key):
         return key in self._content
 
 
@@ -66,28 +68,27 @@ class Wordle:
     def __init__(self):
         self._MAX_ROUNDS = 6
         self._dictionary = _Dict()
-    
-    def _ReInit(self):
-        self._secret_word = self._GetSecretWord()
+
+    def _reinit(self):
+        self._secret_word = self._get_secret_word()
         self._guesses_letters = {
-            letter: _Letter(letter) for letter in string.ascii_uppercase }
+            letter: _Letter(letter) for letter in string.ascii_uppercase}
         self._keyboard = {
-            letter: _Letter(letter) for letter in string.ascii_uppercase }
+            letter: _Letter(letter) for letter in string.ascii_uppercase}
         self._current_round = 0
         self._guesses = [[] for _ in range(self._MAX_ROUNDS)]
 
-    def _GetSecretWord(self):
+    def _get_secret_word(self):
         """
         a method to chose new secret word for the game
         """
-        SECRET_WORD_LENGTH = 5
-        word = self._dictionary.GetRandomWord()
-        while (not word.isalpha()) or (len(word) != SECRET_WORD_LENGTH):
-            word = self._dictionary.GetRandomWord()
+        _SECRET_WORD_LENGTH = 5
+        word = self._dictionary.get_random_word()
+        while (not word.isalpha()) or (len(word) != _SECRET_WORD_LENGTH):
+            word = self._dictionary.get_random_word()
         return word
 
-
-    def _DetermineColor(self, letter):
+    def _determine_color(self, letter):
         color = _Colors.WHITE
         if letter in self._guesses:
             color = _Colors.DIMMED
@@ -95,20 +96,17 @@ class Wordle:
             color += _Colors.GREEN
         return color
 
-
-    def _PrintKeyboard(self):
+    def _print_keyboard(self):
         for letter in self._keyboard.values():
-            letter.Print(end=" ")
-        print() # newline
+            letter.print(end=" ")
+        print()  # newline
 
-
-    def _PrintGuesses(self):
+    def _print_guesses(self):
         for idx, guess in enumerate(self._guesses):
             print(f"{_Colors.DIMMED + _Colors.CYAN}{idx + 1} > {_Colors.RESET}", end="")
             for letter in guess:
-                letter.Print(end=" ")
-            print() # new line
-
+                letter.print(end=" ")
+            print()  # new line
 
     def _analyze_guess(self, guess):
         color = None
@@ -117,61 +115,57 @@ class Wordle:
                 if letter == self._secret_word[idx]:
                     color = _Colors.BOLD_GREEN
                 else:
-                    color = _Colors.BOLD_YELLOW   
+                    color = _Colors.BOLD_YELLOW
             else:
                 color = _Colors.BOLD_RED
 
-            self._guesses[self._current_round].append(_Letter(letter,color))
-            self._keyboard[letter].ChangeColor(_Colors.DIMMED + color)
+            self._guesses[self._current_round].append(_Letter(letter, color))
+            self._keyboard[letter].set_color(_Colors.DIMMED + color)
 
-
-    def _ValidateGuess(self, guess):
-        return (guess.isalpha() and 
+    def _validate_guess(self, guess):
+        return (guess.isalpha() and
                 len(guess) == 5 and
-                self._dictionary.Exists(guess))
+                self._dictionary.exists(guess))
 
-
-    def _PrintWinScreen(self):
+    def _print_win_screen(self):
         print(f"\n{_Colors.BOLD_GREEN}Success!")
         print(f"You have guessed the word ", end="")
         print(f"{_Colors.BOLD_CYAN + self._secret_word} ", end="")
         print(f"{_Colors.BOLD_GREEN}correctly!{_Colors.RESET}")
 
-
-    def _PrintLoseScreen(self):
+    def _print_lose_screen(self):
         print(f"{_Colors.BOLD_RED}Game Over!")
         print(f"The secret word was {_Colors.BOLD_YELLOW + self._secret_word}.")
         print(f"{_Colors.BOLD_MAGENTA}Better luck next time!{_Colors.RESET}")
 
-
-    def Start(self):
+    def start(self):
         """
         Start new game
-        """        
-        self._ReInit()
-        
+        """
+        self._reinit()
+
         was_invalid_guess = False
         has_guessed_correctly = False
         while self._current_round < self._MAX_ROUNDS and not has_guessed_correctly:
-            _ClearScreen()
-            
+            _clear_screen()
+
+            self._print_guesses()
+            self._print_keyboard()
+
             if was_invalid_guess:
-                print(f"{_Colors.BOLD_WHITE}Invalid guess{_Colors.RESET}")
+                print(f"{_Colors.BOLD_RED}Invalid guess{_Colors.RESET}")
                 was_invalid_guess = False
-            
-            self._PrintGuesses()
-            self._PrintKeyboard()
+
             new_guess = input("Enter your guess:\n").upper()
-            if not self._ValidateGuess(new_guess):
+            if not self._validate_guess(new_guess):
                 was_invalid_guess = True
-                
+
             else:
                 has_guessed_correctly = new_guess == self._secret_word
                 self._analyze_guess(new_guess)
                 self._current_round += 1
-                
-                
+
         if has_guessed_correctly:
-            self._PrintWinScreen()
+            self._print_win_screen()
         else:
-            self._PrintLoseScreen()
+            self._print_lose_screen()
